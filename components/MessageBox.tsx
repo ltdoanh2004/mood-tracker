82dfae7bafdc525244cf46d60f8e03c39513e0a8
Châu Thịnh <chauthinh00710@gmail.com>
2024-11-28 19:53:35 +0700

    init commit

---
+++ components/MessageBox.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MessageBox = () => {
+  return (
+    <View>
+      <Text>MessageBox</Text>
+    </View>
+  )
+}
+
+export default MessageBox
+
+const styles = StyleSheet.create({})
